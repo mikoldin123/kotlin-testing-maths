e838e6abda2ca53a385e7af1989b1a5d9bdbe389
mikoldin123 <mikoldin123@gmail.com>
2021-01-15 12:43:51 +0800

    feat: Added percentage
    feat: Added squareroot

--- mathSample.kt
+++ mathSample.kt
@@ -1,18 +1,24 @@
+import kotlin.math.sqrt
+
+
 fun main() {
-   val value = doMaths(Operation.ADDITION, 10.0, 11.0)
+   val value = doMaths(Operation.SQUAREROOT, 10.0)
    
    println(value)
 }
 
 enum class Operation {
     ADDITION,
-    SUBTRACTION 
+    SUBTRACTION,
+    PERCENTAGE,
+    SQUAREROOT
 }
 
-fun doMaths(operation: Operation, value1: Double, value2: Double): Double {
+fun doMaths(operation: Operation, value1: Double, value2: Double = 0.0): Double {
     when(operation) {
         Operation.ADDITION -> return addNumbers(value1, value2)
         Operation.SUBTRACTION -> return subtractNumbers(value1, value2)
+        Operation.SQUAREROOT -> return squareRoot(value1)
     }
 }
 
@@ -24,4 +30,13 @@ fun addNumbers(a: Double, b: Double): Double {
 fun subtractNumbers(a: Double, b: Double): Double {
     var difference = a - b
     return difference
+}
+
+fun percentageOf(value: Double, percent: Double): Double {
+    val percent = value * percent / 100
+    return percent
+}
+
+fun geSquareRoot(number: Double): Double {
+    return sqrt(number)
 }
